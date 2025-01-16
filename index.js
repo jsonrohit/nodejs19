94f15c3d6249899de24858e5c3b0c80b86c98c0e
Rohit13SQFT <rohit.kumar@13sqft.com>
2025-01-16 18:52:20 +0530

    rest client setup & post get api impment

--- index.js
+++ index.js
@@ -23,11 +23,17 @@ const typeDefs = gql`
     id: Int
     title: String
     body: String
+    lastName: String
   }
 
   type Query {
     posts: [Post]
     post(id: Int!): Post
+  }
+
+  type Mutation {
+    createPost(userId: Int!, title: String!, body: String!): Post
+    deletePost(id: Int!): String
   }
 `;
 
@@ -35,26 +41,40 @@ const typeDefs = gql`
 // Define resolvers
 const resolvers = {
     Query: {
-      posts: async () => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        return response.data;
-      },
-      post: async (_, { id }) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-        return response.data;
-      },
+        posts: async () => {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            return response.data;
+        },
+        post: async (_, { id }) => {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            return response.data;
+        },
     },
-  };
+    Mutation: {
+        createPost: async (_, { userId, title, body }) => {
+            const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+                userId,
+                title,
+                body,
+            });
+            return response.data;
+        },
+        deletePost: async (_, { id }) => {
+            await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            return `Post with ID ${id} has been deleted.`;
+        },
+    }
+};
 
-  // Create Apollo Server
+// Create Apollo Server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-  });
+});
 
-  // Start the server
+// Start the server
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
-  });
+});
 
 // app.listen(PORT, () => console.log('server started on port http://localhost:' + PORT))
